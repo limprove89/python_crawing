b71ebc8309b7688174e84f4a6ef6c90fae6baa0c
원영선 <ifyousun@won-yeongseon-ui-MacBookPro.local>
2019-12-09 22:33:48 +0900

    good

--- crawing.py
+++ crawing.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 import urllib.request
 import requests
 import pandas as pd
+import re
+
+f = open('191209_all.csv', 'w')
+f.write('상품명, 가격, 판매처, 링크\n')
 
 target_url = 'http://browse.gmarket.co.kr/list?category=200002525&s=8'
 html = urllib.request.urlopen(target_url).read()
@@ -9,15 +13,147 @@ soup = BeautifulSoup(html, 'html.parser')
 
 for item in soup.select('body .box__item-container .box__information'):
     title = item.select_one('.text__item').text.strip()
-    price = item.select_one('strong.text__value').text
+    price = item.select_one('strong.text__value').text.replace(',','')
     try:
         seller = item.select_one('.text__seller').text
     except:
         seller = "종합몰"
-    results =[title, price, seller]
+    goodscodes = item.select_one('a[href*=goodscode]')
+    goodscode = re.findall(r'\d{5,}', goodscodes['href'])[0]
+    url = 'http://item.gmarket.co.kr/Item?goodscode=' + goodscode      
     
-table = pd.DataFrame(result,columns=('title','price','seller'))
-table.to_csv(path,encoding="cp949", mode="w", index=true)
-makeexel(soup.select, 191209.csv)
-# articles = pd.DataFrame(results, columns=['상품명','가격','판매자'])
-# articles.to_csv('191209.csv')
+    f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
+
+f.close()
+
+f = open('191209_orange.csv', 'w')
+f.write('상품명, 가격, 판매처, 링크\n')
+
+target_url = 'http://browse.gmarket.co.kr/list?category=300012572&s=8'
+html = urllib.request.urlopen(target_url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+for item in soup.select('body .box__item-container .box__information'):
+    title = item.select_one('.text__item').text.strip()
+    price = item.select_one('strong.text__value').text.replace(',','')
+    try:
+        seller = item.select_one('.text__seller').text
+    except:
+        seller = "종합몰"
+    goodscodes = item.select_one('a[href*=goodscode]')
+    goodscode = re.findall(r'\d{5,}', goodscodes['href'])[0]
+    url = 'http://item.gmarket.co.kr/Item?goodscode=' + goodscode      
+    
+    f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
+
+f.close()
+
+f = open('191209_mong.csv', 'w')
+f.write('상품명, 가격, 판매처, 링크\n')
+
+target_url = 'http://browse.gmarket.co.kr/list?category=300012481&s=8'
+html = urllib.request.urlopen(target_url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+for item in soup.select('body .box__item-container .box__information'):
+    title = item.select_one('.text__item').text.strip()
+    price = item.select_one('strong.text__value').text.replace(',','')
+    try:
+        seller = item.select_one('.text__seller').text
+    except:
+        seller = "종합몰"
+    goodscodes = item.select_one('a[href*=goodscode]')
+    goodscode = re.findall(r'\d{5,}', goodscodes['href'])[0]
+    url = 'http://item.gmarket.co.kr/Item?goodscode=' + goodscode      
+    
+    f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
+
+f.close()
+
+f = open('191209_avo.csv', 'w')
+f.write('상품명, 가격, 판매처, 링크\n')
+
+target_url = 'http://browse.gmarket.co.kr/list?category=300018841&s=8'
+html = urllib.request.urlopen(target_url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+for item in soup.select('body .box__item-container .box__information'):
+    title = item.select_one('.text__item').text.strip()
+    price = item.select_one('strong.text__value').text.replace(',','')
+    try:
+        seller = item.select_one('.text__seller').text
+    except:
+        seller = "종합몰"
+    goodscodes = item.select_one('a[href*=goodscode]')
+    goodscode = re.findall(r'\d{5,}', goodscodes['href'])[0]
+    url = 'http://item.gmarket.co.kr/Item?goodscode=' + goodscode      
+    
+    f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
+
+f.close()
+
+f = open('191209_lemon.csv', 'w')
+f.write('상품명, 가격, 판매처, 링크\n')
+
+target_url = 'http://browse.gmarket.co.kr/list?category=300028000&s=8'
+html = urllib.request.urlopen(target_url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+for item in soup.select('body .box__item-container .box__information'):
+    title = item.select_one('.text__item').text.strip()
+    price = item.select_one('strong.text__value').text.replace(',','')
+    try:
+        seller = item.select_one('.text__seller').text
+    except:
+        seller = "종합몰"
+    goodscodes = item.select_one('a[href*=goodscode]')
+    goodscode = re.findall(r'\d{5,}', goodscodes['href'])[0]
+    url = 'http://item.gmarket.co.kr/Item?goodscode=' + goodscode      
+    
+    f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
+
+f.close()
+
+f = open('191209_red.csv', 'w')
+f.write('상품명, 가격, 판매처, 링크\n')
+
+target_url = 'http://browse.gmarket.co.kr/list?category=300012483&s=8'
+html = urllib.request.urlopen(target_url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+for item in soup.select('body .box__item-container .box__information'):
+    title = item.select_one('.text__item').text.strip()
+    price = item.select_one('strong.text__value').text.replace(',','')
+    try:
+        seller = item.select_one('.text__seller').text
+    except:
+        seller = "종합몰"
+    goodscodes = item.select_one('a[href*=goodscode]')
+    goodscode = re.findall(r'\d{5,}', goodscodes['href'])[0]
+    url = 'http://item.gmarket.co.kr/Item?goodscode=' + goodscode      
+    
+    f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
+
+f.close()
+
+f = open('191209_grape.csv', 'w')
+f.write('상품명, 가격, 판매처, 링크\n')
+
+target_url = 'http://browse.gmarket.co.kr/list?category=300025761&s=8'
+html = urllib.request.urlopen(target_url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+for item in soup.select('body .box__item-container .box__information'):
+    title = item.select_one('.text__item').text.strip()
+    price = item.select_one('strong.text__value').text.replace(',','')
+    try:
+        seller = item.select_one('.text__seller').text
+    except:
+        seller = "종합몰"
+    goodscodes = item.select_one('a[href*=goodscode]')
+    goodscode = re.findall(r'\d{5,}', goodscodes['href'])[0]
+    url = 'http://item.gmarket.co.kr/Item?goodscode=' + goodscode      
+    
+    f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
+
+f.close()
