08e8cf4f827620a6e51c878bef2759f538db6ecf
limprove89 <limprove89@gmail.com>
2019-12-10 14:30:32 +0900

    good

--- crawing_gmarket.py
+++ crawing_gmarket.py
@@ -4,8 +4,9 @@ import requests
 import pandas as pd
 import re
 
-f = open('191209_all.csv', 'w')
+f = open('data/191210_gmarket.csv', 'w', encoding='cp949')
 f.write('상품명, 가격, 판매처, 링크\n')
+f.write('종합' + '\n')  
 
 target_url = 'http://browse.gmarket.co.kr/list?category=200002525&s=8'
 html = urllib.request.urlopen(target_url).read()
@@ -24,10 +25,13 @@ for item in soup.select('body .box__item-container .box__information'):
     
     f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
 
-f.close()
+f.write('\n') 
+f.write('오렌지' + '\n')   
 
-f = open('191209_orange.csv', 'w')
-f.write('상품명, 가격, 판매처, 링크\n')
+# f.close()
+
+# f = open('191210_orange.csv', 'w', encoding='cp949')
+# f.write('상품명, 가격, 판매처, 링크\n')
 
 target_url = 'http://browse.gmarket.co.kr/list?category=300012572&s=8'
 html = urllib.request.urlopen(target_url).read()
@@ -46,10 +50,13 @@ for item in soup.select('body .box__item-container .box__information'):
     
     f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
 
-f.close()
+f.write('\n')
+f.write('자몽' + '\n')   
 
-f = open('191209_mong.csv', 'w')
-f.write('상품명, 가격, 판매처, 링크\n')
+# f.close()
+
+# f = open('191210_mong.csv', 'w', encoding='cp949')
+# f.write('상품명, 가격, 판매처, 링크\n')
 
 target_url = 'http://browse.gmarket.co.kr/list?category=300012481&s=8'
 html = urllib.request.urlopen(target_url).read()
@@ -68,10 +75,13 @@ for item in soup.select('body .box__item-container .box__information'):
     
     f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
 
-f.close()
+f.write('\n')
+f.write('아보카도' + '\n')   
 
-f = open('191209_avo.csv', 'w')
-f.write('상품명, 가격, 판매처, 링크\n')
+# f.close()
+
+# f = open('191210_avo.csv', 'w', encoding='cp949')
+# f.write('상품명, 가격, 판매처, 링크\n')
 
 target_url = 'http://browse.gmarket.co.kr/list?category=300018841&s=8'
 html = urllib.request.urlopen(target_url).read()
@@ -90,10 +100,13 @@ for item in soup.select('body .box__item-container .box__information'):
     
     f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
 
-f.close()
+f.write('\n')
+f.write('레몬' + '\n')   
 
-f = open('191209_lemon.csv', 'w')
-f.write('상품명, 가격, 판매처, 링크\n')
+# f.close()
+
+# f = open('191210_lemon.csv', 'w', encoding='cp949')
+# f.write('상품명, 가격, 판매처, 링크\n')
 
 target_url = 'http://browse.gmarket.co.kr/list?category=300028000&s=8'
 html = urllib.request.urlopen(target_url).read()
@@ -112,10 +125,13 @@ for item in soup.select('body .box__item-container .box__information'):
     
     f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
 
-f.close()
+f.write('\n')
+f.write('석류' + '\n')   
 
-f = open('191209_red.csv', 'w')
-f.write('상품명, 가격, 판매처, 링크\n')
+# f.close()
+
+# f = open('191210_red.csv', 'w', encoding='cp949')
+# f.write('상품명, 가격, 판매처, 링크\n')
 
 target_url = 'http://browse.gmarket.co.kr/list?category=300012483&s=8'
 html = urllib.request.urlopen(target_url).read()
@@ -134,10 +150,12 @@ for item in soup.select('body .box__item-container .box__information'):
     
     f.write(title + ',' + price + ',' + seller + ',' + url + '\n')
 
-f.close()
+f.write('\n')
+f.write('포도' + '\n')   
+# f.close()
 
-f = open('191209_grape.csv', 'w')
-f.write('상품명, 가격, 판매처, 링크\n')
+# f = open('191210_grape.csv', 'w', encoding='cp949')
+# f.write('상품명, 가격, 판매처, 링크\n')
 
 target_url = 'http://browse.gmarket.co.kr/list?category=300025761&s=8'
 html = urllib.request.urlopen(target_url).read()
